fadc768727420ff71beba9eb57fbb6d22d902c5f
Khloe Hou <khou@e1z3r2p5.42.us.org>
2019-02-18 01:16:58 -0800

    02/18/2019

--- libft/libft_basic/ft_atoi_base.c
+++ libft/libft_basic/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:27:59 by khou              #+#    #+#             */
-/*   Updated: 2019/02/18 01:08:25 by khou             ###   ########.fr       */
+/*   Updated: 2019/02/18 01:15:53 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	valid_char(char c)
 {
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+	if (ft_isalnum(c))
 		return (1);
 	return (0);
 }
 
-int	ft_atoi_base(const char *str, int str_base)
+int		ft_atoi_base(const char *str, int str_base)
 {
 	int nbr;
 	int sign;
@@ -39,5 +39,5 @@ int	ft_atoi_base(const char *str, int str_base)
 		*str >= 'A' && *str <= 'F' ? nbr += *str - 'A' + 10 : 0;
 		str++;
 	}
-	return (sign ? nbr = sign * nbr : nbr);	
+	return (sign ? nbr = sign * nbr : nbr);
 }
